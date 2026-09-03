f98ec1bb73ca30ada80331a5127e6558891da09a
Nikhil <nikhil@cautio.in>
2026-09-03 18:55:57 +0530

    Implement system information retrieval and JSON output formatting

--- internal/system/system.go
+++ internal/system/system.go
@@ -1,1 +1,20 @@
 package system
+
+import (
+	"github.com/nikhil25803/purrpeek/internal/system/platform"
+)
+
+type SystemInfo struct {
+	OS *platform.OSInformation
+}
+
+func GetSystemInformation() (*SystemInfo, error) {
+	osInfo, err := platform.GetOSInformation()
+	if err != nil {
+		return nil, err
+	}
+
+	return &SystemInfo{
+		OS: osInfo,
+	}, nil
+}
