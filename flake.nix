29ce056873208e391a3b5b76689d3a3631b6c345
Nikhil <nikhil25803@gmail.com>
2026-09-04 22:02:03 +0530

    feat: add initial flake.nix configuration for Nix package management

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Your system information, from a cat's perspective";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.buildGoModule {
+            pname = "purrpeek";
+            version = "0.1.0";
+
+            src = ./.;
+
+            vendorHash = null;
+
+            meta = {
+              description = "Your system information, from a cat's perspective";
+              homepage = "https://github.com/nikhil25803/purrpeek";
+              license = nixpkgs.lib.licenses.mit;
+              mainProgram = "purrpeek";
+            };
+          };
+        }
+      );
+    };
+}
